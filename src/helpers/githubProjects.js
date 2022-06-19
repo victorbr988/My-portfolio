92444670a9176fd143ba053506f4e5941f164e88
victor gabriel <gabrielvghs666@gmail.com>
2022-06-19 17:03:48 -0300

    feat: add new projects in page work

--- src/helpers/githubProjects.js
+++ src/helpers/githubProjects.js
@@ -1,6 +1,7 @@
-import trivia from '../assets/images/trivia.jpeg'
+import trivia from '../assets/images/trivia.png'
 import imageTest from '../assets/images/imageTest.jpeg'
 import shoppingOnline from '../assets/images/shopping-online.png'
+import wallet from '../assets/images/wallet.png'
 export const githubProjects = [
   {
     id: Date(),
@@ -15,7 +16,7 @@ export const githubProjects = [
     id: Date(),
     Category: 'Teste',
     title: 'Testes unitários',
-    description: 'Projeto com testes unitários para iniciantes em javaScipt consguirem praticar lógica',
+    description: 'Projeto com testes unitários para iniciantes em javaScipt consguirem praticar lógica.',
     image: imageTest,
     Web: '',
     github: 'https://github.com/victorbr988/Pratique-js'
@@ -24,9 +25,18 @@ export const githubProjects = [
     id: Date(),
     Category: 'Vanilla',
     title: 'Shopping online',
-    description: 'Primeiro Projeto desenvolvido consumindo API do mercado Livre',
+    description: 'Primeiro Projeto desenvolvido consumindo API do mercado Livre.',
     image: shoppingOnline,
     Web: 'https://victorbr988.github.io/Shopping-Online/',
     github: 'https://github.com/victorbr988/Shopping-Online'
+  },
+  {
+    id: Date(),
+    Category: 'React.js',
+    title: 'Carteira digital',
+    description: 'Aplicação de uma carteira digital onde é possível ter o controle de suas finanças.',
+    image: wallet,
+    Web: 'https://carteira-digital-six.vercel.app/',
+    github: 'https://github.com/victorbr988/Carteira-Digital'
   }
 ]
