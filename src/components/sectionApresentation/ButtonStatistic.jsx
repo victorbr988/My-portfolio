1d175beac6f8297977ac60527d53be0d3934bd1f
Victor Gabriel <gabrielvghs666@gmail.com>
2022-08-29 08:09:45 -0300

    style: replace animation buttons in section apresentation

--- src/components/sectionApresentation/ButtonStatistic.jsx
+++ src/components/sectionApresentation/ButtonStatistic.jsx
@@ -3,7 +3,7 @@ export const ButtonStatistic = ({href, children}) => {
     <a
       target="_blank"
       href={href}
-      className='flex hover:scale-105 transition cursor-pointer shadow-lg text-white items-center gap-2 bg-orange-400 rounded-lg px-4 py-2 justify-around'
+      className='flex hover:scale-105 transition cursor-pointer shadow-lg text-white items-center gap-2 bg-orange-400 rounded-lg px-4 py-2 justify-evenly'
     >
       {children}
     </a>
--- src/components/sectionApresentation/SocialMedia.jsx
+++ src/components/sectionApresentation/SocialMedia.jsx
@@ -4,29 +4,29 @@ export const SocialMedia = () => {
     <div className="flex gap-3 flex-col mt-10">
       <section className="">
         <a 
-          className="flex shadow-lg group w-10 hover:w-full hover:scale-105 transition-all gap-2 border-2 justify-center items-center border-orange-400 p-2 rounded-full"
+          className="flex shadow-lg group lg:w-10 w-full hover:w-full hover:scale-105 transition-all gap-2 border-2 justify-center items-center border-orange-400 p-2 rounded-full"
           href="https://github.com/victorbr988"
         >
           <FiGithub className="text-xl" />
-          <p className="hidden group-hover:block">Github</p>
+          <p className="lg:hidden group-hover:block">Github</p>
         </a>
       </section>
       <section>
         <a 
-          className="flex group w-10 hover:w-full hover:scale-105 transition-all shadow-lg gap-2  border-2 justify-center items-center border-orange-400 p-2 rounded-full"
+          className="flex group lg:w-10 w-full hover:w-full hover:scale-105 transition-all shadow-lg gap-2  border-2 justify-center items-center border-orange-400 p-2 rounded-full"
           href="https://www.instagram.com/gab_ulquiorrasbtn/"
         >
           <FiInstagram className="text-lg" />
-          <p className="hidden group-hover:block">Instagram</p>
+          <p className="lg:hidden group-hover:block">Instagram</p>
         </a>
       </section>
       <section>
         <a 
-          className="flex group w-10 hover:w-full hover:scale-105 transition-all shadow-lg gap-2  border-2 justify-center items-center border-orange-400 p-2 rounded-full"
+          className="flex group lg:w-10 w-full hover:w-full hover:scale-105 transition-all shadow-lg gap-2  border-2 justify-center items-center border-orange-400 p-2 rounded-full"
           href="https://www.linkedin.com/in/victor-gabriel-dev"
         >
           <FiLinkedin className="text-xl" />
-          <p className="hidden group-hover:block">LinkedIn</p>
+          <p className="lg:hidden group-hover:block">LinkedIn</p>
         </a>
       </section>  
     </div>
